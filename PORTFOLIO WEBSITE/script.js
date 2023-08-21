661b42f6ebdf994a60dd63f8b0f3264094096f90
Tanya Gupta <127372130+tanishka098@users.noreply.github.com>
2023-08-21 20:24:31 +0530

    Add files via upload

---
+++ PORTFOLIO WEBSITE/script.js
@@ -0,0 +1,7 @@
+/*typing animation */
+var typed = new Typed(".typing", {
+    strings:[" Frontend Web Designer", " Frontend Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
